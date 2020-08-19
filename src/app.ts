6053361e2617693cdb31d5cfb14de2f00a766afa
Rotimi Babalola <tolurotimi@hotmail.com>
2020-08-19 11:42:53 +0200

    Emit new user event

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,34 @@
-console.log('Hello world Rotimi');
+/* eslint-disable no-console */
+import express from 'express';
+import cors from 'cors';
+import socketIO from 'socket.io';
+import { IUser } from './types';
+
+const PORT = 5000;
+const app = express();
+
+app.use(cors());
+
+const server = app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
+
+const io = socketIO(server);
+
+const activeUsers = new Set();
+
+// TODO: Fix any types
+io.on('connection', (socket: any) => {
+  console.log('Made socket connection');
+
+  socket.on('new user', (data: IUser) => {
+    const { userId, userName } = data;
+    activeUsers.add({ userId, userName });
+    io.emit('new user', [...activeUsers]);
+  });
+
+  socket.on('disconnect', () => {
+    activeUsers.delete(socket.userId);
+    io.emit('user disconnected', socket.userId);
+  });
+});
